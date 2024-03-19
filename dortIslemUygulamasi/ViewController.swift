6ca52ceba02d2b290f3e6a07f163585d38c37827
barisaydgdu <barisaydogdu76@gmail.com>
2024-03-19 23:15:25 +0300

    The application is done.

--- dortIslemUygulamasi/ViewController.swift
+++ dortIslemUygulamasi/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstText: UITextField!
+    
+    @IBOutlet weak var sonucLabel: UILabel!
+    @IBOutlet weak var secondText: UITextField!
+    
+    var result = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    // uygulamanın crash vermemesi için forge wrappingleri if ile kontrol ediyoruz.
+    @IBAction func toplamaTiklandi(_ sender: Any) {
+        if let firstNum = Int(firstText.text!) {
+            if let secondNum = Int(secondText.text!){
+                result = firstNum + secondNum
+                sonucLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func cikarmatiklsndi(_ sender: Any) {
+        if let firstNum = Int(firstText.text!) {
+            if let secondNum = Int(secondText.text!){
+                result = firstNum - secondNum
+                sonucLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func bolmeTiklandi(_ sender: Any) {
+        if let firstNum = Int(firstText.text!) {
+            if let secondNum = Int(secondText.text!){
+                result = firstNum / secondNum
+                sonucLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func carpmaTiklandi(_ sender: Any) {
+        if let firstNum = Int(firstText.text!) {
+            if let secondNum = Int(secondText.text!){
+                result = firstNum * secondNum
+                sonucLabel.text = String(result)
+            }
+        }
+    }
+    
+    
 }
 
